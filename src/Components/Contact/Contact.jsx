35c11d73de06ad1d77de97116fe7d23c82a23f7c
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-17 09:42:10 +0200

    packages are uninstalled due animation are will be added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Contact.css';
 import msg_icon from '../../assets/msg-icon.png';
+import mail_icon from '../../assets/mail-icon.png';
+import phone_icon from '../../assets/phone-icon.png';
+import location_icon from '../../assets/location-icon.png';
+import white_arrow from '../../assets/white-arrow.png';
 
 const Contact = () => {
   return (
@@ -9,12 +13,23 @@ const Contact = () => {
         <h3>Send us a message<img src={msg_icon}></img></h3>
         <p>Feel free to reachh out through contact from or find our contact information below. Your feedback, questionns and suggestions are important to us as we are strive to provide exceptional service to our university community.</p>
         <ul>
-            <li>Contact@dev.gamil.com</li>
-            <li>+1 123-456-789</li>
-            <li>77 Massachusetts Ave, Cambride <br /> MA 02139, United States</li>
+            <li><img src={mail_icon}></img> Contact@dev.gamil.com</li>
+            <li><img src={phone_icon}></img> +1 123-456-789</li>
+            <li><img src={location_icon}></img> 77 Massachusetts Ave, Cambride <br /> MA 02139, United States</li>
         </ul>
       </div>
-      <div className='contact-col'></div>
+      <div className='contact-col'>
+        <form>
+            <label>Your Name</label>
+            <input type='text' name='name' placeholder='Enter your name' required />
+            <label>Phone Number</label>
+            <input type='tel' name='phone' placeholder='Enter your phone number' required />
+            <label>Write your message</label>
+            <textarea name='message' rows="6" placeholder='message' />
+            <button type='submit' className='btn dark-btn'>Submit <img src={white_arrow}></img>  </button>
+        </form>
+        <span>sending</span>
+      </div>
     </div>
   )
 }
