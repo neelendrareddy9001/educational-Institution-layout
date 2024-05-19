113f65c688d58043dc0d215e5d3727f1a3e51970
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-19 16:59:24 +0200

    video palyer added in about component and additional styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Contact from "./Components/Contact/Contact";
@@ -8,8 +9,10 @@ import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
 import Testimonial from "./Components/Testimonial/Testimonial";
 import Title from "./Components/Title/Title";
+import VideoPayler from "./Components/VideoPlayer/VideoPayler";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
   return (
     <div>
       <Navbar />
@@ -17,7 +20,7 @@ const App = () => {
       <div className="container">
         <Title  subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle='Gallery' title='Campus Photo' />
         <Campus />
         <Title subTitle='TESTIMONIALS' title='Campus Photos' />
@@ -26,6 +29,7 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
+      <VideoPayler playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import "./Navbar.css";
 import Logo from '../../assets/logo.png';
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-scroll";
 import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
   const [mobileMenu, setMobileMenu] = useState(false);
-
+  const closeMenu = useRef(null);
+  const closeMobileMenu = (e) => {
+    if(e.target === closeMenu.current) {
+      setMobileMenu(false)
+    }
+  }
   useEffect(() => {
     window.addEventListener('scroll', () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false)
@@ -19,7 +24,7 @@ const Navbar = () => {
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={Logo} alt="" className="logo" />
-      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'} ref={closeMenu} onClick={closeMobileMenu}>
         <li><Link to="hero" smooth={true} spy={true} activeClass="active" offset={0} duration={500}>Home</Link></li>
         <li><Link to="programs" smooth={true} spy={true} offset={-260} activeClass="active" duration={500}>Programs</Link></li>
         <li><Link to="about" smooth={true} spy={true} offset={-130} activeClass="active" duration={500} >About Us</Link></li>
