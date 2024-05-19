3d6f4606f6fe6da9ad80c79ba7b8ebfbfc23fd35
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-05-19 06:20:33 +0200

    all are updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import Logo from '../../assets/logo.png';
 import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -14,12 +15,12 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={Logo} alt="" className="logo" />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className="btn">Contact Us</button></li>
+        <li><Link to="hero" smooth={true} offset={0} duration={500}></Link> Home</li>
+        <li><Link to="programs" smooth={true} offset={-260}></Link> Program</li>
+        <li><Link to="about" smooth={true} offset={-150} duration={500} ></Link> About Us</li>
+        <li><Link to="campus" smooth={true} offset={-260} duration={500} ></Link> Campus</li>
+        <li><Link to="testimonials" smooth={true} offset={-260} duration={500} ></Link> Testimonials</li>
+        <li><Link to="contact" smooth={true} offset={-260} duration={500} ></Link> <button className="btn">Contact Us</button></li>
       </ul>
     </nav>
   )
